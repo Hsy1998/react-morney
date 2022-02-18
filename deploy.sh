1adfb11d2350dc04525a850e56159b35045f2943
黄思煜 <553331998@qq.com>
2022-02-18 23:13:19 +0800

    添加deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,9 @@
 
 yarn build &&
 cd build &&
-gut init &&
+git init &&
 git add . &&
 git commit -m 'deploy' &&
-git branch -M main &&
-git remote add origin git@github.com:Hsy1998/React-Pocket-book.git &&
-git push -u origin main &&
+git remote add origin git@github.com:Hsy1998/react-money-page.git &&
+git push -u origin master -f 
 cd -
